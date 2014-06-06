618b49a343184a44a3b715d2ce85d5c65e9a1883
Heather <heatheramahan@gmail.com>
2014-06-06 13:04:31 -0700

    modified lettercount to remove embedded for loop. added upper vs lowercase count functionality. modularized code.

--- lettercount.py
+++ lettercount.py
@@ -1,27 +1,48 @@
 from sys import argv
 
+def create_tally_list(length):
+	tally_list = []
+	for n in range(length):
+		tally_list.append(0)
+	return tally_list
+
+
+def count_lower_chars(fileobj):
+	# lowercase ascii decimals: 97 - 122
+	tally_list = create_tally_list(26)
+	for char in fileobj:
+		if ord(char) in range(97, 123):
+			tally_list[ord(char) - 97] += 1
+	return tally_list
+
+
+
+def count_upper_chars(fileobj):
+	# uppercase ascii decimals: 65 - 90
+	tally_list = create_tally_list(26)
+	for char in fileobj:
+		if ord(char) in range(65, 91):
+			tally_list[ord(char) - 65] += 1
+	return tally_list
 
 
 def main():
 	script, filename = argv
 	f = open(filename)
 
-	f_content = f.read().lower()
+	f_content = f.read()
+
+	num_lower_chars = count_lower_chars(f_content)
+
+	num_upper_chars = count_upper_chars(f_content)
+
+	num_alpha_chars = num_lower_chars + num_upper_chars
 
 
-	alpha_ords = []
-	for n in range (97, 122):
-		alpha_ords.append([n, 0])
+	for num in num_alpha_chars:
+		print num
 
-
-	for char in f_content:
-			for num in alpha_ords:
-				if ord(char) == num[0]:
-					num[1] += 1
-
-
-	for num in alpha_ords:
-		print num[1]
+	f.close()
 
 
 if __name__ == "__main__":
